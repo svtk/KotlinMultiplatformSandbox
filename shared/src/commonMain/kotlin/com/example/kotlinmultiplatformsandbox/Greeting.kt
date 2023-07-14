9e26a52cf8b97b0006e33e8f0e8d73e50ea86f32
svetlana.isakova <svetlana.isakova@jetbrains.com>
2023-07-14 10:17:10 +0200

    Step "Update the UI"

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -5,8 +5,8 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        return if (Random.nextBoolean()) "Hi!" else "Hello!" +
-            "\nGuess what it is! > ${platform.name.reversed()}!"
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
     }
 }
