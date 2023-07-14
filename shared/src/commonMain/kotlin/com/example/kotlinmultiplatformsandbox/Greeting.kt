cc59f08e975866e51886fc79535c40a288b42b72
svetlana.isakova <svetlana.isakova@jetbrains.com>
2023-07-14 23:44:54 +0200

    Step "Explore the project"

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinmultiplatformsandbox
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Guess what it is! > ${platform.name.reversed()}!"
+        return if (Random.nextBoolean()) "Hi!" else "Hello!" +
+            "\nGuess what it is! > ${platform.name.reversed()}!"
     }
 }
